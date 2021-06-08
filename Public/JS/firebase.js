f0d5ac28f306f059738e4169cdf9aa2584e01ebe
Gijs windows <gvbeusekom84@hotmail.com>
2021-06-08 14:53:22 +0200

    landing

---
+++ Public/JS/firebase.js
@@ -0,0 +1,13 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD2SkxyjMM6A1k266yXaBHSmlRZH9P9EW8",
+    authDomain: "deccos-af9fd.firebaseapp.com",
+    databaseURL: "https://deccos-af9fd.firebaseio.com",
+    projectId: "deccos-af9fd",
+    storageBucket: "deccos-af9fd.appspot.com",
+    messagingSenderId: "1072308252691",
+    appId: "1:1072308252691:web:715cfef3053fad28fe8fe6",
+    measurementId: "G-87RH3H603C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
